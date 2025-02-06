100a709130601285e3ef6784357738b1b1132942
qalqa <andreybas04@gmail.com>
2025-02-07 01:30:58 +0300

    nesting part2 cycle complete

--- cycles_nesting.py
+++ cycles_nesting.py
@@ -58,6 +58,73 @@
 #                 if e ** 5 == sum_powers:
 #                     print(a, b, c, d, e)
 
+
+# n = int(input())
+# cnt = 0
+# for i in range(1, n + 1):
+#     for j in range(1, i + 1):
+#         cnt += 1
+#         print(cnt, end=' ')
+#     print()
+
 # n = int(input())
 # for i in range(1, n + 1):
-#     for j in range(1, ):
+#     for j in range(1, i + 1):
+#         print(j, end='')
+#     for j in range(i - 1, 0, -1):
+#         print(j, end='')
+#     print()
+
+# max_sm = 0
+# max_divider = 0
+# a, b = int(input()), int(input())
+# for i in range(a, b + 1):
+#     sm_divs = 0
+#     for j in range(1, i + 1):
+#         if i % j == 0:
+#             sm_divs += j
+#             if sm_divs >= max_sm:
+#                 max_sm = sm_divs
+#                 max_divider = i
+# print(max_divider, max_sm)
+
+# n = int(input())
+#
+# for i in range(1, n + 1):
+#     print(i, end='')
+#     for j in range(1, i + 1):
+#         if i % j == 0:
+#             print("+", end='')
+#     print()
+
+# n = int(input())
+#
+# while n >= 10:
+#     sm = 0
+#     while n > 0:
+#         sm += n % 10
+#         n = n // 10
+#     n = sm
+# print(n)
+
+# n = int(input())
+#
+# sm = 0
+# for i in range(1, n + 1):
+#     fac = 1
+#     for j in range(1, i + 1):
+#         fac *= j
+#     sm += fac
+#
+# print(sm)
+
+a, b = int(input()), int(input())
+
+for i in range(a, b + 1):
+    flag = True
+    for j in range(2, i):
+        if i % j == 0:
+            flag = False
+            break
+    if flag and i != 1:
+        print(i)
